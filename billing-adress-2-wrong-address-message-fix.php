77f38ebeb186c74b457a9b2bba89a6c45adbbfb5
Szymon Jakób <145180594+szymonjakob@users.noreply.github.com>
2023-09-16 10:57:15 +0200

    Update and rename billing-adress-2-wrong-address-message-fix to billing-adress-2-wrong-address-message-fix.php

--- billing-adress-2-wrong-address-message-fix.php
+++ billing-adress-2-wrong-address-message-fix.php
@@ -1,3 +1,4 @@
+<?php
 // EXPERIMENTAL add billing_adress-2 field from checkout -> CSS display: none -> fix wrong address message in cart if you hide it
 add_filter( 'woocommerce_checkout_fields' , 'override_checkout_fields' );
 function override_checkout_fields( $fields ) {
