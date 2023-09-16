4de78e588481f3098fb44935dcbfb63a8feeb4e3
Szymon Jakób <145180594+szymonjakob@users.noreply.github.com>
2023-09-16 10:59:22 +0200

    Update dequeue-wc-blocks-style-from-front-page.php

--- dequeue-wc-blocks-style-from-front-page.php
+++ dequeue-wc-blocks-style-from-front-page.php
@@ -1,7 +1,6 @@
 <?php
 // If you dont use woocommerce blocks dequeue woocommerce blocks style which affect on performance.
 add_action( 'wp_enqueue_scripts', 'disable_woocommerce_block_styles' );
-
 function disable_woocommerce_block_styles() {
     if ( is_front_page() ) {    
         wp_dequeue_style( 'wc-blocks-style' );
