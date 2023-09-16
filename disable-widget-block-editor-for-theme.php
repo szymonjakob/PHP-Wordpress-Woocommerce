2589c39739c0b12699c35330f2cea0173333bfdc
Szymon Jakób <145180594+szymonjakob@users.noreply.github.com>
2023-09-16 10:59:14 +0200

    Update disable-widget-block-editor-for-theme.php

--- disable-widget-block-editor-for-theme.php
+++ disable-widget-block-editor-for-theme.php
@@ -1,6 +1,5 @@
 <?php
 add_action( 'after_setup_theme', 'disable_widget_block_editor_theme_support' );
-
 function disable_widget_block_editor_theme_support() {
     remove_theme_support( 'widgets-block-editor' );
 }
