0f5e6c423c68d20937b2e5d606179c0187b279bf
Szymon Jakób <145180594+szymonjakob@users.noreply.github.com>
2023-12-20 22:22:46 +0100

    Create order-details-email.php

---
+++ order-details-email.php
@@ -0,0 +1,15 @@
+// Add apaczka shipping details to order emails
+add_filter('woocommerce_email_order_meta_fields', 'custom_add_email_meta', 10, 3 );
+function custom_add_email_meta( $fields, $order ) {
+
+    $apaczka_delivery_point = $order->get_meta( 'apaczka_delivery_point' );
+
+    if( ! empty( $apaczka_delivery_point ) ) {
+        $fields[ 'apaczka_delivery_point' ] = array(
+        'label' => 'Punkt odbioru',
+        'value' => ucfirst( $apaczka_delivery_point['apm_supplier'] ) . ' ' . $apaczka_delivery_point['apm_access_point_id']
+        );
+    }
+
+    return $fields;
+}
