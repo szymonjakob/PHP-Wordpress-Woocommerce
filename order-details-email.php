cd45e449bede42e155bc72a7a2bbf585cbce5d8e
Szymon Jakób <145180594+szymonjakob@users.noreply.github.com>
2023-12-21 06:17:15 +0100

    Added missing filter param

--- order-details-email.php
+++ order-details-email.php
@@ -1,13 +1,11 @@
 // Add apaczka shipping details to order emails
 add_filter('woocommerce_email_order_meta_fields', 'custom_add_email_meta', 10, 3 );
-function custom_add_email_meta( $fields, $order ) {
+function custom_add_email_meta( $fields, $sent_to_admin, $order ) {
 
-    $apaczka_delivery_point = $order->get_meta( 'apaczka_delivery_point' );
-
-    if( ! empty( $apaczka_delivery_point ) ) {
-        $fields[ 'apaczka_delivery_point' ] = array(
+    if ( $delivery_point = $order->get_meta('apaczka_delivery_point') ) {
+        $fields['apaczka_delivery_point'] = array(
         'label' => 'Punkt odbioru',
-        'value' => esc_attr( $apaczka_delivery_point['apm_supplier'] ) . ' ' . esc_attr( $apaczka_delivery_point['apm_access_point_id'] )
+        'value' => esc_attr( $delivery_point['apm_supplier'] ) . ' ' . esc_attr( $delivery_point['apm_access_point_id'] )
         );
     }
 
